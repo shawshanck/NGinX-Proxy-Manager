a0ba19a3ac99743b32eeef85fd96210d6e00b7f7
Mohammad Mohammadpour <116471351+shawshanck@users.noreply.github.com>
2023-08-04 09:55:26 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -241,7 +241,7 @@ startInstall()
         mkdir -p docker/nginx-proxy-manager
         cd docker/nginx-proxy-manager
 
-        curl https://gitlab.com/bmcgonag/docker_installs/-/raw/main/docker_compose.nginx_proxy_manager.yml -o docker-compose.yml >> ~/docker-script-install.log 2>&1
+        curl https://raw.githubusercontent.com/shawshanck/NGinX-Proxy-Manager/main/nginx_proxy_manager.yml -o docker-compose.yml >> ~/docker-script-install.log 2>&1
 
         echo "    2. Running the docker-compose.yml to install and start NGinX Proxy Manager"
         echo ""
